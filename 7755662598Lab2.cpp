fb50fd1f5c3bd4778da4b9743fa7c03d5ea5e6d9
TaliseBakerM <bakermat@usc.edu>
2026-01-30 14:34:30 -0800

    whatever

---
+++ 7755662598Lab2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+// Build matrix class
+Class Matrix { 
+
+public: 
+	// These are the attrubutes
+	int value[10][10];
+	int target_row_idx;
+	int minimum;
+	int maximum;	
+	
+	
+	// This is the constructor
+	Matrix {
+		target_row_idx = -1;
+		minimum = 0;
+		maximum = 0;
+	
+	}
+	
+	
+	
+	// Method to read the data in the .txt files
+	void readFile(
+	
+	
+
+
+
+
+};
---
+++ README.txt
@@ -0,0 +1,8 @@
+Name: Talise Baker-Matsuoka
+ID: 7755662598
+Email: bakermat@usc.edu
+
+
+
+Outside helpL
+- I used https://www.w3schools.com/cpp/ref_fstream_fstream.asp for help with how to read a file. 
